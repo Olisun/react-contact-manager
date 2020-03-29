8a24f333553edd5e4d111a0137d13ec294ab0aa5
Oliver Sun <olisun@mac.com>
2020-03-29 13:08:50 -0700

    created components for contact-list and contact-form.js and added these to newly created contact-list and contact-form pages

---
+++ src/pages/contact-list-page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContactList from '../components/contact-list';
+
+const ContactListPage = () => {
+  return (
+    <div>
+      <h1>List of Contacts</h1>
+    </div>
+  );
+};
+
+export default ContactListPage;
