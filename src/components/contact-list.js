edec2568cf86a8a458aa34cad42b977a1d58372a
Oliver Sun <olisun@mac.com>
2020-03-29 18:49:16 -0700

    set up nav menu and routes in app.js. imported fomantic-ui and browser router in index.js

--- src/components/contact-list.js
+++ src/components/contact-list.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ContactList() {
   return (
     <div>
-      <h1>No contacts yet</h1>
+      <p>No contacts yet</p>
     </div>
   );
 }
--- src/pages/contact-list-page.js
+++ src/pages/contact-list-page.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import ContactList from '../components/contact-list';
 
 const ContactListPage = () => {
   return (
     <div>
       <h1>List of Contacts</h1>
+      <ContactList />
     </div>
   );
 };
