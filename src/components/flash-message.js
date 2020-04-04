92d13c73512e12a9eac4784bc5586f48c1f33f8f
Oliver Sun <olisun@mac.com>
2020-04-03 19:53:16 -0700

    added a flash-message component to display a message when starting the app that the db is not connected.  added that flash component to contact-context.js as a case in the reducer function.  added a try-catch to contact-list-page.js to catch the error

---
+++ src/components/flash-message.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Message } from 'semantic-ui-react';
+
+export default function FlashMessage({ message }) {
+  return (
+    <Message
+      positive={message.type === 'success'}
+      negative={message.type === 'fail'}
+      header={message.title}
+      contect={message.content}
+    />
+  );
+}
+
+export function flashErrorMessage(dispatch, error) {
+  // check to see if server or network error
+  const err = error.response ? error.response.data : error;
+  dispatch({
+    type: 'FLASH_MESSAGE',
+    payload: {
+      type: 'fail',
+      title: err.name,
+      content: err.message,
+    },
+  });
+}
--- src/pages/contact-list-page.js
+++ src/pages/contact-list-page.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 import ContactList from '../components/contact-list';
 import { ContactContext } from '../context/contact-context';
+import FlashMessage, { flashErrorMessage } from '../components/flash-message';
 
 
 export default function ContactListPage() {
@@ -10,11 +11,15 @@ export default function ContactListPage() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await axios.get('http://localhost:3030/contacts');
-      dispatch({
-        type: 'FETCH_CONTACTS',
-        payload: response.data.data || response.data, // in case pagenation is disabled
-      });
+      try {
+        const response = await axios.get('http://localhost:3030/contacts');
+        dispatch({
+          type: 'FETCH_CONTACTS',
+          payload: response.data.data || response.data, // in case pagenation is disabled
+        });
+      } catch (error) {
+        flashErrorMessage(dispatch, error);
+      }
     };
     fetchData();
   }, [dispatch]);
@@ -22,6 +27,7 @@ export default function ContactListPage() {
   return (
     <div>
       <h1>List of Contacts</h1>
+      {state.message.content && <FlashMessage message={state.message} />}
       <ContactList contacts={state.contacts} />
     </div>
   );
